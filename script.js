ca453215295be9d0742664fc7c897719e8060e3d
Kacper Klimczak <kacper_klimczak_2609@protonmail.com>
2023-02-22 20:41:52 +0100

    add animations, working project, mobile first

--- script.js
+++ script.js
@@ -10,10 +10,15 @@ next.addEventListener('click',()=>{
     tanyaSlide.classList.add('vanish');
     johnImg.classList.remove('vanish');
     johnSlide.classList.remove('vanish');
+    next.style.opacity = .5;
+    prev.style.opacity = 1;
+    johnImg.opacity = 1;
 })
 prev.addEventListener('click',()=>{
     tanyaImg.classList.remove('vanish');
     tanyaSlide.classList.remove('vanish');
     johnImg.classList.add('vanish');
     johnSlide.classList.add('vanish');
+    next.style.opacity = 1;
+    prev.style.opacity = .5;
 })
